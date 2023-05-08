5a5629c6f08cfde1aaae2b152449c0db20c7157b
dennisshum <108983859+dennisshum@users.noreply.github.com>
2023-05-08 13:40:19 +0000

    Add ConversationEntityMemory

--- chatbotGPT.py
+++ chatbotGPT.py
@@ -1,23 +1,23 @@
 import os
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-import streamlit as st
+from langchain.memory import ConversationEntityMemory
+from langchain.memory.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
 
 os.getenv("OPENAI_API_KEY")
 
 llm = ChatOpenAI(temperature=0.9)
 
-memory = ConversationBufferMemory()
+memory = ConversationEntityMemory(llm=llm)
 
-chain = ConversationChain(llm=llm, memory=memory, verbose=False)
+chain = ConversationChain(llm=llm, memory=memory, prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE, verbose=False)
 
 print("Howdy!")
 
 while True:
   user_input = input("> ")
 
-  ai_response = chain.run(user_input)
+  response = chain.run(user_input)
 
-  print("\nAssistant:\n", ai_response)
+  print("\nAssistant:\n", response)
 
